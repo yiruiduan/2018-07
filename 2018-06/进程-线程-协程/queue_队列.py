6dc1fbedf5a4753cedada0fb8e1194960b6e9c29
yiruiduan <yiruiduan@126.com>
2018-06-08 17:25:19 +0800

    进程线程的学习

--- 2018-06/进程-线程-协程/queue_队列.py
+++ 2018-06/进程-线程-协程/queue_队列.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#队列数据仅有一份取走了就没有了
 import queue
+#last in first out
+q=queue.LifoQueue()
+q.put(1)
+q.put(2)
+q.put(3)
+print(q.get_nowait())
+print(q.get_nowait())
+print(q.get_nowait())
+
+#设置优先级
+q1=queue.PriorityQueue()
+q1.put("yiruiduan",-1)
+q1.put("zhangye",3)
+q1.put("yinuo",10)
+print(q1.get_nowait())
+print(q1.get_nowait())
+print(q1.get_nowait())
---
+++ 2018-06/进程-线程-协程/生产者消费者模型.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import threading,time,queue
+
+q = queue.Queue(maxsize=10)
+
+
+def Porducer():
+    count=1
+    while True:
+        q.put("骨头%s"%count)
+        print("生产了骨头",count)
+        count+=1
+        time.sleep(2)
+
+def Consumer(name):
+    while q.qsize()>0:
+        print("[%s] 取到 %s并且吃了它"%(name,q.get()))
+        time.sleep(1)
+
+p=threading.Thread(target=Porducer)
+p.start()
+c=threading.Thread(target=Consumer,args=("wangcai",))
+c.start()
+c1=threading.Thread(target=Consumer,args=("xiaogou",))
+c1.start()
