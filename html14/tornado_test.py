3a3fe91e003038b4979b1cc40ef938ace14c0825
yiruiduan <yiruiduan@126.com>
2018-12-14 17:49:23 +0800

    study html

---
+++ html14/tornado_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import tornado.web
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        # user=self.get_argument("user")
+        # pwd=self.get_argument("pwd")
+        sex=self.get_argument("sex")
+        fav=self.get_arguments("fav")
+        file=self.get_argument("file")
+        meno=self.get_argument("meno")
+        city=self.get_arguments("city")
+        print(sex,fav,meno,city)
+        self.write("ok")
+        # if user=="yiruiduan" and pwd=="530487":
+        #     self.write("OK")
+        # else:
+        #     self.write("gun")
+    def post(self):
+        print("123")
+        self.write("POST")
+application=tornado.web.Application([("/index",MainHandler),])
+
+if __name__=="__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
