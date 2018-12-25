eccc1333cf682c1b0ff985c50abd8a9301b053f2
yiruiduan <yiruiduan@126.com>
2018-12-25 09:57:18 +0800

    study html

--- iroad/add_user.py
+++ iroad/add_user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import time
 from selenium import webdriver
 import xlrd
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-with open ("exec_test.txt","r",encoding="utf-8") as f,\
-    open ("exec1_test.txt","r",encoding="utf-8") as f1:
-    for line in f:
-        # print(line.strip().split()[4])
-        if line.strip().split()[4] == "1.2M":
-            print(line.strip().split()[-1])
-    print("#########################")
-    for line1 in f1:
-        if line1.strip().split()[4] == "1.2M":
-            print(line1.strip().split()[-1])
+# with open ("exec_test.txt","r",encoding="utf-8") as f,\
+#     open ("exec1_test.txt","r",encoding="utf-8") as f1:
+#     for line in f:
+#         # print(line.strip().split()[4])
+#         if line.strip().split()[4] == "1.2M":
+#             print(line.strip().split()[-1])
+#     print("#########################")
+#     for line1 in f1:
+#         if line1.strip().split()[4] == "1.2M":
+#             print(line1.strip().split()[-1])
+import selenium
